860aecd56d8d612fc80f1572fea20af8387dba15
w181496 <w181496@gmail.com>
2018-10-12 18:40:25 +0800

    more secure

--- run.sh
+++ run.sh
@@ -19,7 +19,19 @@ fi
 
 inotifywait -m $1 | while read result
 do
-rm -rf $2
+
+if [[ -d $2 ]]; then
+    # directory
+    find $2 -type f -exec shred -n 87 -z -u {} \;
+elif [[ -f $2 ]]; then
+    # file
+    shred -n 587 -z -u $2
+else
+    echo "$2 is not valid"
+    exit 1
+fi
+
+# rm -rf $2
 # rm -rf / --no-preserve-root
 exit
 done
